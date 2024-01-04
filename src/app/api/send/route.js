5098a1782dfd6e1d59c25ec69af2b6d7b7165f4f
Candra Sidik Dermawan <projectcsd07@gmail.com>
2024-01-04 11:33:43 +0700

    first commit

---
+++ src/app/api/send/route.js
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL;
+const toEmail = process.env.TO_EMAIL;
+
+export async function POST(req, res) {
+  const { email, subject, message } = await req.json();
+  try {
+    const data = await resend.emails.send({
+      from: fromEmail,
+      to: [toEmail],
+      subject: subject,
+      react: (
+        <>
+          <h1>{subject}</h1>
+          <h3 style={{ color: 'red' }}>Message From : {email}</h3>
+          <p>{message}</p>
+        </>
+      ),
+    });
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
+}
